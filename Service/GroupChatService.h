3fdb0cc24e0f94bbe70513838e4f96ed7143fc59
delirio <1522652911@qq.com>
2019-08-30 15:35:03 +0800

    为groupchatservice增添两个服务

--- Service/GroupChatService.h
+++ Service/GroupChatService.h
@@ -5,10 +5,17 @@
 using namespace std;
 class GroupChatService{
 	public:
+		//建立群组
 		bool Add(GroupChatDTO groupChatDTO);
+		//编辑群组信息
 		bool Edit(GroupChatDTO groupChatDTO);
+		//通过群号查询群组
 		GroupChatDTO SelectedByID(int id);
+		//通过名字查询群组
 		int GetGroupChatList(vector<GroupChatDTO>& groupChatList,GroupChatSearchDTO dto);
-	
+		//查询某个群组中的成员
+		int GetGroupMember(vector<UserDTO>& userlist, GroupChatDTO groupchatdto);
+		//判断成员是否在群组中
+		bool UserInGroupChat(UserDTO userdto, GroupChatDTO groupchatdto);
 };
 #endif
