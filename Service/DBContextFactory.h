2243979fdb884ed22f63efd49f890b47648bbdcf
delirio <1522652911@qq.com>
2019-09-01 20:06:14 +0800

    一点修改

---
+++ Service/DBContextFactory.h
@@ -0,0 +1,22 @@
+#ifndef DBCONTEXTFACTORY_H
+#define DBCONTEXTFACTORY_H
+#include <string>
+#include <vector>
+#include <sqlpp11/mysql/mysql.h>
+#include <sqlpp11/sqlpp11.h>
+namespace mysql=sqlpp::mysql;
+
+class DBContextFactory{
+public:
+    static mysql::connection& Instance(){
+      auto config=std::make_shared<mysql::connection_config>();
+      config->password="mypassword";
+      config->user="root";
+      config->debug=true;
+      config->database="PoppopDatabase";
+      static mysql::connection db(config);
+      return db;
+    }
+};
+
+#endif
