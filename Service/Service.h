04a444afb83dcbbaefff3b6e2fa693250400a753
delirio <1522652911@qq.com>
2019-09-02 10:55:15 +0800

    一点小修改

--- Service/Service.h
+++ Service/Service.h
@@ -2,13 +2,10 @@
 #define SERVICE_H
 
 //函数接口
-#include"DBContextFactory.h"
 #include"FriendService.h"
 #include"GroupChatService.h"
 #include"GroupService.h"
 #include"MessageService.h"
-#include"MyTable.h"
-#include"SplitStr.h"
 #include"UserService.h"
 
 #endif
