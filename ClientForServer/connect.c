03ed48e287b79c033b4eb084c6e333e41a981cb1
Eric <43440918+Ericsheart@users.noreply.github.com>
2019-09-02 18:28:18 +0800

    Update and rename Connect.c to connect.c

--- ClientForServer/connect.c
+++ ClientForServer/connect.c
@@ -1,12 +1,18 @@
-#include "Connect.h"
+#include "socket.h"
+
+const int MAX_LISTEN = 5; // 最大监听数
+const int SERVER_PORT = 8088; // 端口
+const char SERVER_IPV4[] = "127.0.0.1"; // ip地址
+const int MAX_BUF = 1024; // 缓冲区大小
+
+int fd = -1; // socked通道
 
 void ErrorHandling(char* message){
     perror(message);
     exit(EXIT_FAILURE);
 }
 
-int Connect(){ 
-    int sockfd;
+void Connect(){ 
     struct sockaddr_in client_addr;
 
     bzero(&client_addr, sizeof(client_addr));
@@ -17,16 +23,12 @@ int Connect(){
         ErrorHandling("ip error");
 
     // 采用TCP协议
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         ErrorHandling("Socekt error");
     puts("socket created");
 
     // 连接
-    if(connect(sockfd, (struct sockaddr*) &client_addr, sizeof(client_addr))==-1)
+    if(connect(fd, (struct sockaddr*) &client_addr, sizeof(client_addr))==-1)
         ErrorHandling("Connect error");
     puts("server connected");
-
-    return sockfd;
-    // close(sockfd);
-    // 关闭连接
 }
