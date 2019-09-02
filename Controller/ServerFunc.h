a1c3cb7f21cfcc77a372691e65b0abe72dd56acc
weilijing <1799936358@qq.com>
2019-09-02 21:09:37 +0800

    修改一些连接服务层的接口

---
+++ Controller/ServerFunc.h
@@ -0,0 +1,11 @@
+#ifdef ServerFunc_h
+#define ServerFunc_h
+bool CreateUser(int client_fd);
+bool Create(int client_fd);
+bool Reply(int client_fd);
+bool Apply(int client_fd);
+bool Quit(int client_fd);
+bool Login(int client_fd);
+void ErrorHandling(char *message); // 错误处理函数
+void *ServerForClient(void *arg);
+#endif;
