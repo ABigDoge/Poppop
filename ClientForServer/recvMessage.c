252f433a01ea7425ef68837ed5c71fba476542ce
Eric <43440918+Ericsheart@users.noreply.github.com>
2019-09-02 21:04:01 +0800

    Delete recvMessage.c

--- ClientForServer/recvMessage.c
+++
@@ -1,8 +0,0 @@
-#include "socket.h"
-
-char* const RecvMessege(){
-    char* const buf = (char*)malloc((MAX_BUF + 1) * sizeof(char));
-    bzero(buf, MAX_BUF + 1);
-    int len = recv(fd, buf, MAX_BUF, 0);
-    return buf;
-}
