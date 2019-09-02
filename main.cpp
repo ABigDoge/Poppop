40c08f2e9e05444cbcb983c4cdab4908a700fa1b
delirio <1522652911@qq.com>
2019-09-02 15:28:21 +0800

    一点修改

--- main.cpp
+++ main.cpp
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 2013 - 2016, Roland Bock
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "Service.h"
 #include "DTO.h"
 //#include"SplitStr.h"
@@ -86,6 +61,9 @@ int main()
         << "   password: " << userDTO.PassWord << endl;
     }
 
+    //UserService::PutUserOnline 成功
+    UserService::PutUserOnline(7, 1234);
+
     //GroupService::Add 成功
     GroupDTO groupDTO;
     groupDTO.Name = "默认分组";
@@ -101,7 +79,7 @@ int main()
     friendDTO.Group_ID = groupid;
     FriendService::Add(friendDTO);
     cout << friendDTO.That_ID << " is a friend of " << friendDTO.This_ID << " in group " 
-        << friendDTO.Group_ID << endl;
+        << friendDTO.Group_ID << endl;       
 
   }
   catch (const sqlpp::exception& e)
