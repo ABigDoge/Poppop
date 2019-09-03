6c09f587665497c009b779f762768b3e85f54bb3
weilijing <1799936358@qq.com>
2019-09-03 12:58:16 +0800

    为Socket传输定义的结构体，请前端注意查看

--- DTO/UserDTO.h
+++ DTO/UserDTO.h
@@ -5,16 +5,16 @@ using namespace std;
 //存储用户个人信息
 class UserDTO{   
 public:
-	 int ID;		//用户id
-	 string Name;    //用户姓名
-	 string PassWord;   //密码
-	 string Sex;     //性别
-	 string Image;   //头像
-	 string Department_Name;  //所在部门
-	 string Motto;   //个性签名
-	 unsigned int IP_Addr;		//ip地址
-	 int Online_State;	//在线状态
-	 int Msg_to_recv;     //待接收信息数量
+	int ID;		//用户id
+	string Name;    //用户姓名
+	string PassWord;   //密码
+	string Sex;     //性别
+	string Image;   //头像
+ 	string Department_Name;  //所在部门
+	string Motto;   //个性签名
+	unsigned int IP_Addr;		//ip地址
+	int Online_State;	//在线状态
+	//int Msg_to_recv;     //待接收信息数量
 	//UserDTO(){}
 };
 #endif
