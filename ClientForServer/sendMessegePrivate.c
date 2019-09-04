eea67e42eedc046847d0e902996433ab3f6b86b1
weilijing <1799936358@qq.com>
2019-09-04 13:28:52 +0800

    修改发消息的接口

--- ClientForServer/sendMessegePrivate.c
+++ ClientForServer/sendMessegePrivate.c
@@ -1,12 +1,12 @@
 #include "socket.h"
 
-int SendMessegePrivate(const struct MessagePrivate* data){
+int SendMessegePrivate(struct MessagePrivate *data){
 
     if (send(fd, "send", MAX_BUF, 0) <= 0)
-		return 0;
+		  return 0;
 
     // 发数据
     if (send(fd, (char*)&data,sizeof(data), 0) <= 0)
-		return 0;
+		  return 0;
 
 }
